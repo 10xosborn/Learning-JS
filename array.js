d31203c8b97301c4c3d6ce4333084f432c401aef
Osborn Tulasi <osborntulasi@gmail.com>
2025-10-08 08:44:48 +0000

    Initial commit: Learning JS

---
+++ array.js
@@ -0,0 +1,18 @@
+const numbers = [1, 2, 3, 4, 5];
+
+//map method to double each number in the array
+const doubled = numbers.map(num => num * 2);
+
+//filter method to get only even numbers
+const filtered = numbers.filter(num => num % 2 === 0);
+
+//reduce method to sum all numbers in the array
+const sum = numbers.reduce((total, num) => total + num, 0);
+
+//find method to get the first number greater than 3
+const found = numbers.find(num => num > 30);
+
+// console.log('Doubled:', doubled);
+ console.log('Filtered (Even NUmbers):', filtered);
+// console.log ('Sum:', sum);
+// console.log('Found (First nummbers > 3):', found);
---
+++ try.js
@@ -0,0 +1,5 @@
+const names = ['Alice', 'Bob', 'Charlie'];
+
+for (const name of names) {
+    console.log(name);
+}
